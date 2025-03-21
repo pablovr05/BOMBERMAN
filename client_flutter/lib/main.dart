5eb6ac2e15a6991e1d46290af528e9980092527f
pablo <pablovicenteroura2005@gmail.com>
2025-03-21 16:23:40 +0100

    commit

--- client_flutter/lib/main.dart
+++ client_flutter/lib/main.dart
@@ -1,21 +1,10 @@
-import 'dart:io'; // Para verificar la plataforma
 import 'package:flutter/cupertino.dart';
 import 'package:provider/provider.dart';
-import 'package:window_size/window_size.dart'; // Asegúrate de importar esto
 import 'app_data.dart';
 import 'app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
-  // Verifica si la plataforma es de escritorio
-  if (Platform.isWindows || Platform.isMacOS || Platform.isLinux) {
-    // Configura el tamaño de la ventana
-    setWindowTitle('Mi Aplicación Flutter');
-    setWindowMinSize(const Size(600, 400)); // Tamaño mínimo de la ventana
-    setWindowMaxSize(const Size(800, 600)); // Tamaño máximo de la ventana
-    setWindowSize(const Size(800, 600)); // Tamaño inicial de la ventana
-  }
 
   runApp(
     ChangeNotifierProvider(
